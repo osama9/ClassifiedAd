27ede022e1bbba2e3979ec5c267ec962df5d02f9
osama9 <osama.747@gmail.com>
2015-02-04 21:33:37 +0300

    Delete Angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require angular
-//= require angular-route
 //= require bootstrap/dist/js/bootstrap
 //= require login-signup-modal-window/js/main
-//= require classified_ad
-//= require classified_ad.route
+
+//= require_tree ./templates
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :classified_ads
 
   root "classified_ads#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
