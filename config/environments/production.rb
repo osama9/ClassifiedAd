c529b5e333d2330a7c728a102f8c78c7ff771e54
osama9 <osama.747@gmail.com>
2015-02-09 07:33:42 +0300

    Fixing rails_admin gen issue with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  cconfig.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 end
